f4627b28e93e35f2290feabf57b47e06c0cbe063
Adam Simpson <adam@adamsimpson.net>
2021-11-29 09:12:28 -0500

    refactor: exec out to ffmpeg

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module lofibar
 
 go 1.16
 
-require github.com/getlantern/systray v1.1.0 // indirect
+require (
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/getlantern/systray v1.1.0 // indirect
+	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2 // indirect
+)
--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	_ "embed"
+	"encoding/json"
 	"log"
+	"net/http"
 	"os/exec"
+	"strings"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/getlantern/systray"
 )
 
@@ -13,12 +17,12 @@ var icon []byte
 var cmd *exec.Cmd
 var isPlaying bool
 
-func playPause() {
+func playPause(url string) {
 	if isPlaying {
 		cmd.Process.Kill()
 		isPlaying = false
 	} else {
-		cmd = exec.Command("mpv", "https://www.youtube.com/watch?v=5qap5aO4i9A", "--no-video")
+		cmd = exec.Command("ffmpeg", "-i", url, "-f", "pulse", "lofi")
 		err := cmd.Start()
 
 		if err != nil {
@@ -26,6 +30,47 @@ func playPause() {
 		}
 		isPlaying = true
 	}
+}
+
+func parseYT() (streamURL string) {
+	type StreamPayload struct {
+		StreamingData struct {
+			HlsManifestUrl   string
+			ExpiresInSeconds string
+		}
+	}
+
+	resp, err := http.Get("https://www.youtube.com/watch?v=5qap5aO4i9A")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doc.Find("script").Each(func(i int, s *goquery.Selection) {
+		if strings.Contains(s.Text(), "var ytInitialPlayerResponse") {
+			jSSlice := strings.Split(s.Text(), "var ytInitialPlayerResponse = ")
+			jsonBytes := []byte(strings.TrimSuffix(jSSlice[1], ";"))
+			var stream = &StreamPayload{}
+
+			err := json.Unmarshal(jsonBytes, stream)
+
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			streamURL = stream.StreamingData.HlsManifestUrl
+		}
+	})
+
+	return streamURL
 }
 
 func onReady() {
@@ -40,7 +85,8 @@ func onReady() {
 		for {
 			select {
 			case <-mPlay.ClickedCh:
-				playPause()
+				url := parseYT()
+				playPause(url)
 			case <-mQuit.ClickedCh:
 				systray.Quit()
 			}
